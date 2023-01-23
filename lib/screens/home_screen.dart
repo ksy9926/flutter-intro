e8f8fa6d4f0a61dd0b82ad12afe2b41684f6ba92
ksy9926 <ksy9926@naver.com>
2023-01-24 03:23:03 +0900

    Webtoon: Webtoon Card

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -24,44 +24,86 @@ class HomeScreen extends StatelessWidget {
       ),
       body: FutureBuilder(
         future: webtoons,
+        // builder는 UI를 그려주는 함수이다.
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            // ListView.builder 기능 + 구분자를 넣어주는 ListView
-            return ListView.separated(
-              scrollDirection: Axis.horizontal,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                var webtoon = snapshot.data![index];
-                return Text(webtoon.title);
-              },
-              separatorBuilder: (context, index) => const SizedBox(
-                width: 20,
+            return Column(children: [
+              const SizedBox(
+                height: 50,
               ),
-            );
-
-            // 조금 더 최적화된 ListView
-            // return ListView.builder(
-            //   scrollDirection: Axis.horizontal,
-            //   itemCount: snapshot.data!.length,
-            //   itemBuilder: (context, index) {
-            //     var webtoon = snapshot.data![index];
-            //     return Text(webtoon.title);
-            //   },
-            // );
-
-            // 기본적인 ListView
-            // return ListView(
-            //   children: [
-            //     for (var webtoon in snapshot.data!) Text(webtoon.title),
-            //   ],
-            // );
+              Expanded(
+                child: makeList(snapshot),
+              )
+            ]);
           }
           return const Center(
             child: CircularProgressIndicator(),
           );
         },
       ),
-      // builder는 UI를 그려주는 함수이다.
     );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    // ListView.builder 기능 + 구분자를 넣어주는 ListView
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      padding: const EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 20,
+      ),
+      itemCount: snapshot.data!.length,
+      itemBuilder: (context, index) {
+        var webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              width: 250,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 15,
+                      offset: const Offset(10, 10),
+                      color: Colors.black.withOpacity(0.5),
+                    )
+                  ]),
+              // clipBehavior - 자식의 부모 영역 침범을 제어하는 방법
+              clipBehavior: Clip.hardEdge,
+              child: Image.network(webtoon.thumb),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              webtoon.title,
+              style: const TextStyle(
+                fontSize: 22,
+              ),
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(
+        width: 40,
+      ),
+    );
+
+    // 조금 더 최적화된 ListView
+    // return ListView.builder(
+    //   scrollDirection: Axis.horizontal,
+    //   itemCount: snapshot.data!.length,
+    //   itemBuilder: (context, index) {
+    //     var webtoon = snapshot.data![index];
+    //     return Text(webtoon.title);
+    //   },
+    // );
+
+    // 기본적인 ListView
+    // return ListView(
+    //   children: [
+    //     for (var webtoon in snapshot.data!) Text(webtoon.title),
+    //   ],
+    // );
   }
 }
